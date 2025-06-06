54386dc6128c2ea758c3fa4adb7a4912675e8711
BarteG45 <bartosz.graczkowski@wp.pl>
2025-06-06 14:42:29 +0200

    Definicje podłączone

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const synonymsField = document.getElementById("synonyms");
 const antonymsField = document.getElementById("antonyms");
 const searchButton = document.getElementById("search_button");
 const keywordInput = document.getElementById("keyword");
+const definition = document.getElementById("definition");
+
 
 const APINinjasKey = "cXRL+1hx7MFwH/CcnJNt6w==ifTKw1ZncXBSDZ7Q";
 const SynAntURL = "https://api.api-ninjas.com/v1/thesaurus?word=";
@@ -48,4 +50,33 @@ function fetchSynonymsAntonyms(){
     })
 }
 
-searchButton.addEventListener("click", fetchSynonymsAntonyms);
+searchButton.addEventListener("click", () => {
+    fetchSynonymsAntonyms();
+    fetchDefinition();
+});
+
+
+
+function fetchDefinition() {
+    const inputValue = keywordInput.value.trim().replaceAll(" ", "+");
+    const DefinitionURL = "https://api.api-ninjas.com/v1/dictionary?word=" + inputValue;
+
+    fetch(DefinitionURL, {
+        headers: {
+            'X-Api-Key': APINinjasKey
+        }
+    })
+    .then(response => response.json())
+    .then(response => {
+        console.log("Definition response:", response);
+        if (response.definition) {
+            definition.innerText = response.definition;
+        } else {
+            definition.innerText = "No definition found.";
+        }
+    })
+    .catch(error => {
+        console.error("Błąd podczas pobierania definicji:", error);
+        definition.innerText = "Error fetching definition.";
+    });
+}
